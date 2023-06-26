95608a1cf9662b6529e0d8de886ee08e74082184
aaronmrice <372839+aaronmrice@users.noreply.github.com>
2023-06-25 22:43:16 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ impl Solution {
     pub fn is_match(input: String, pattern: String) -> bool {
         dbg!(&pattern);
         let pattern = parse_pattern(&pattern[..]);
-        dbg!(pattern);
-        unimplemented!();
+        dbg!(&pattern);
+
+        dbg!(string_matches_regex(&input, &pattern))
     }
 }
 
@@ -15,7 +16,10 @@ enum RegexpToken {
     Star(char),
     WildcardStar,
 }
-fn parse_pattern(pattern: &str) -> Vec<RegexpToken> {
+
+type Regexp = Vec<RegexpToken>;
+
+fn parse_pattern(pattern: &str) -> Regexp {
     let mut ret: Vec<RegexpToken> = vec![];
     let chars: Vec<char> = pattern.chars().collect();
     let mut i = 0;
@@ -38,6 +42,57 @@ fn parse_pattern(pattern: &str) -> Vec<RegexpToken> {
 
     ret
 }
+/*
+In order to apply backtracking to a specific class of problems,
+one must provide the data P for the particular instance of the problem that is to be solved,
+and six procedural parameters, root, reject, accept, first, next, and output.
+These procedures should take the instance data P as a parameter and should do the following:
+
+root(P): return the partial candidate at the root of the search tree.
+reject(P,c): return true only if the partial candidate c is not worth completing.
+accept(P,c): return true if c is a solution of P, and false otherwise.
+first(P,c): generate the first extension of candidate c.
+next(P,s): generate the next alternative extension of a candidate, after the extension s.
+output(P,c): use the solution c of P, as appropriate to the application.
+The backtracking algorithm reduces the problem to the call backtrack(root(P)), where backtrack is the following recursive procedure:
+
+procedure backtrack(P, c) is
+    if reject(P, c) then return
+    if accept(P, c) then output(P, c)
+    s ← first(P, c)
+    while s ≠ NULL do
+        backtrack(P, s)
+        s ← next(P, s)
+*/
+
+enum PartialRegexpToken {
+    Literal(char),
+    Wildcard(char),
+    Star(char, u32),
+    WildcardStar(u32),
+}
+type RegexpPartialSolution = Vec<PartialRegexpToken>;
+/// return the partial candidate at the root of the search tree.
+fn root(inp: &str, regexp: &Regexp) -> PartialSolution {
+    Vec::new()
+}
+/// return true only if the partial candidate c is not worth completing.
+fn accept(inp: &str, regexp: &Regexp, partial: &PartialSolution) -> bool {
+    for 
+}
+/// return true only if the partial candidate c is not worth completing.
+fn reject(inp: &str, regexp: &Regexp, partial: &PartialSolution) -> bool {}
+/// generate the first extension of candidate c.
+fn first(inp: &str, regexp: &Regexp, partial: &PartialSolution) -> &PartialSolution {}
+/// generate the next alternative extension of a candidate, after the extension s.
+fn next(inp: &str, regexp: &Regexp, partial: &PartialSolution) -> &PartialSolution {}
+
+fn string_matches_regex(string: &str, regexp: &[RegexpToken]) -> bool {
+    let stack: Vec<PartialSolution> = Vec::new();
+}
+
+fn backtrack(problem: P, candidate: C) {}
+
 fn main() {
     assert_eq!(false, Solution::is_match("aa".into(), "a".into()));
     assert_eq!(false, Solution::is_match("aa".into(), "a*".into()));
